bd6d7adcf786216034c623073875593aa6de2b1e
ThachSacKda <Kdatsgcc210007@fpt.edu.vn>
2024-06-20 01:38:01 +0700

    show user and action

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,72 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss'
+import { getAllUsers } from '../../services/userService';
+
 class UserManage extends Component {
 
-    state = {
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers: []
 
+        }
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await getAllUsers('All');
+        if(response && response.errCode ===0){
+            this.setState({
+                arrUsers: response.users
+            })
+        }
+        
     }
 
 
     render() {
+        console.log('check render', this.state)
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-container">
+                <div className='title text-center mt-5'>User Management</div>
+                <div className='user-table mt-5 mx-5'>
+                    <table id="customers">
+                        <tr>
+                            <th>Email</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Address</th>
+                            <th>Action</th>
+                        </tr>
+                        
+                            { arrUsers && arrUsers.map((item,index) => {
+                                console.log('Kda check map', item, index)
+                                return(
+                                    <tr>
+                                        <td>{item.email}</td>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.address}</td>
+                                        <td>
+                                            <button className='btn-edit'><i className="fa-solid fa-pencil"></i></button>
+                                            <button className='btn-delete'><i className="fa-solid fa-trash"></i></button>
+                                        </td>
+                                    
+                                    </tr>
+                                   
+                                )
+                            })
+
+                            }
+                            
+                        
+                    </table>
+
+                </div>
+            </div>
+
         );
     }
 
