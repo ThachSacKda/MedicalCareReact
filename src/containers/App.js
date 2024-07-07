2bd4ed6f0d4394a063172a563a42f20fe33f91ad
ThachSacKda <Kdatsgcc210007@fpt.edu.vn>
2024-07-07 22:13:21 +0700

    slider img

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,6 +18,10 @@ import Login from './Auth/Login';
 
 import { CustomToastCloseButton } from '../components/CustomToast';
 import HomePage from './HomePage/HomePage';
+
+
+import CustomScrollbars from '../components/CustomScrollbars';
+
 class App extends Component {
 
     handlePersistorState = () => {
@@ -45,14 +49,16 @@ class App extends Component {
                     <div className="main-container">
                         {this.props.isLoggedIn && <Header />}
 
-                        <span className="content-container">
+                        <div className="content-container">
+                        <CustomScrollbars style={{height:'100vh', width: '100%'}}>
                             <Switch>
                                 <Route path={path.HOME} exact component={(Home)} />
                                 <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
                                 <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
                                 <Route path={path.HOMEPAGE} component={HomePage} />
                             </Switch>
-                        </span>
+                        </CustomScrollbars>
+                        </div>
 
                         <ToastContainer
                             className="toast-container" toastClassName="toast-item" bodyClassName="toast-item-body"
--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import HomeHeader from './HomeHeader'
+import Specialty from './Section/Specialty';
+
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+</head>
 
 class HomePage extends Component {
 
     render() {
         return (
+            <div>
             <HomeHeader/>
+            <Specialty/>
+            <div style={{height: '300px'}}>
+
+            </div>
+            </div>
+            
         );
     }
 
