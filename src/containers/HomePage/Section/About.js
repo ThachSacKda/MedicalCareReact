bf603dbcdec715e8814a3e9e4697a11237c2938e
ThachSacKda <Kdatsgcc210007@fpt.edu.vn>
2024-08-09 21:48:08 +0700

    about

--- src/containers/HomePage/Section/About.js
+++ src/containers/HomePage/Section/About.js
@@ -1,29 +1,64 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import '../HomePage.scss'
+import '../HomePage.scss';
 import { FormattedMessage } from 'react-intl';
-import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css"
-import specialtyImg from '../../../assets/specialty/bone.jpg'
-import { LANGUAGES } from '../../../utils';
-
-import {changeLanguageApp} from "../../../store/actions"
-
 
 class About extends Component {
-
-
     render() {
         return (
-            <div className='section-share section-about'>
-                 
-                About Us
-            </div>
-            
+            <section id="service" className="section-padding">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-4 col-sm-4">
+                            <h2 className="ser-title">Our Service</h2>
+                            <hr className="botm-line" />
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris cillum.</p>
+                        </div>
+                        <div className="col-md-4 col-sm-4">
+                            <div className="service-info">
+                                <div className="icon">
+                                    <i className="fa fa-stethoscope"></i>
+                                </div>
+                                <div className="icon-info">
+                                    <h4>24 Hour Support</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                </div>
+                            </div>
+                            <div className="service-info">
+                                <div className="icon">
+                                    <i className="fa fa-ambulance"></i>
+                                </div>
+                                <div className="icon-info">
+                                    <h4>Emergency Services</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4 col-sm-4">
+                            <div className="service-info">
+                                <div className="icon">
+                                    <i className="fa fa-user-md"></i>
+                                </div>
+                                <div className="icon-info">
+                                    <h4>Medical Counseling</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                </div>
+                            </div>
+                            <div className="service-info">
+                                <div className="icon">
+                                    <i className="fa fa-medkit"></i>
+                                </div>
+                                <div className="icon-info">
+                                    <h4>Premium Healthcare</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         );
     }
-
 }
 
 const mapStateToProps = state => {
@@ -34,8 +69,7 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-    };
+    return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(About);
