7ace7de34fcf94760a7cf03d2ffb80e955f31146
ThachSacKda <Kdatsgcc210007@fpt.edu.vn>
2024-08-10 21:24:39 +0700

    split admin and doctor role

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -2,55 +2,77 @@ export const adminMenu = [
     { //quan li nguoi dung
         name: 'menu.admin.manage-user', menus: [
             {
-                name: 'menu.admin.crud', link: '/system/user-manage' ,
-                
+                name: 'menu.admin.crud', link: '/system/user-manage',
+
             },
             {
-                name: 'menu.admin.crud-redux', link: '/system/user-redux' ,
-                
+                name: 'menu.admin.crud-redux', link: '/system/user-redux',
+
             },
             {
-                name: 'menu.admin.manage-doctor', link: '/system/manage-doctor' ,
+                name: 'menu.admin.manage-doctor', link: '/system/manage-doctor',
                 // subMenus: [
                 //     { name: 'menu.system.system-administrator.user-manage', link: '/system/user-manage' },
                 //     { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
                 // ]
             },
-            {
-                name: 'menu.admin.manage-admin', link: '/system/user-admin' ,
-                
+            // {
+            //     name: 'menu.admin.manage-admin', link: '/system/user-admin' ,
+
+            // },
+
+            { //quan li ke hoach kham benh cua bac si
+               
+                        name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule',       
             },
-           
+
         ]
     },
     { //quan li phòng khám
         name: 'menu.admin.clinic',
         menus: [
             {
-                name: 'menu.admin.manage-clinic', link: '/system/manage-clinic' ,
-                
+                name: 'menu.admin.manage-clinic', link: '/system/manage-clinic',
+
             },
-           
+
         ]
     },
     { //quan li chuyên khoa
         name: 'menu.admin.speciality',
         menus: [
             {
-                name: 'menu.admin.manage-speciality', link: '/system/manage-speciality' ,
-                
+                name: 'menu.admin.manage-speciality', link: '/system/manage-speciality',
+
             },
-           
+
         ]
     },
     { //quan li cẩm nang
         name: 'menu.admin.handbook',
         menus: [
             {
-                name: 'menu.admin.manage-handbook', link: '/system/manage-handbook' ,
-                
+                name: 'menu.admin.manage-handbook', link: '/system/manage-handbook',
+
             },
-           
+
         ]
     },
+];
+
+export const doctorMenu = [
+    {
+        name: 'menu.admin.manage-user',
+        menus: [
+             //quan li ke hoach kham benh cua bac si
+                    {
+                        name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule',
+
+                    },
+
+
+                
+            
+        ]
+    }
 ];
