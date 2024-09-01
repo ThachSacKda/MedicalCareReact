7e6b96b7a9011e9180005ea834c3a7424cdc4767
ThachSacKda <Kdatsgcc210007@fpt.edu.vn>
2024-09-01 18:32:46 +0700

    show detail specialty

--- src/containers/Patient/Specialty/DetailSpecialty.js
+++ src/containers/Patient/Specialty/DetailSpecialty.js
@@ -3,37 +3,53 @@ import { connect } from "react-redux";
 import { FormattedMessage } from 'react-intl';
 import './DetailSpecialty.scss'
 import HomeHeader from '../../HomePage/HomeHeader'
-
-
+import DoctorSchedule from '../Doctor/DoctorSchedule';
+import DoctorExtraInfor from '../Doctor/DoctorExtraInfor';
+import ProfileDoctor from '../Doctor/ProfileDoctor';
 
 class DetailSpecialty extends Component {
     constructor(props) {
         super(props);
         this.state = {
-           
+            arrDoctorId: [4, 5, 6]
         }
     }
 
-    async componentDidMount() { }
+    async componentDidMount() {}
 
-    async componentDidUpdate(prevProps, prevState, snapshot) {
-         
-    }
-
-    showHideDetailInfor = () => {
-        this.setState({
-            isShowDetailInfor: !this.state.isShowDetailInfor  // Toggle detail info visibility
-        });
-    }
+    async componentDidUpdate(prevProps, prevState, snapshot) {}
 
     render() {
-
-
+        let { arrDoctorId } = this.state;
         return (
-            <>
-            <HomeHeader/>
-           <div>Hello detail specialty</div>
-           </>
+            <div className='detail-specialty-container'>
+                <HomeHeader />
+                <div className='specialty-description'>
+                    {/* Nội dung mô tả chuyên khoa */}
+                </div>
+                {arrDoctorId && arrDoctorId.length > 0 &&
+                    arrDoctorId.map((item, index) => {
+                        return (
+                            <div className='each-doctor' key={index}>
+                                <div className='doctor-content'>
+                                    <div className='content-left'>
+                                        <ProfileDoctor
+                                            doctorId={item}
+                                            isshowDesciptionDoctor={true}
+                                        />
+                                    </div>
+                                    <div className='content-right'>
+                                        <DoctorSchedule
+                                            doctorIdFromParent={item}
+                                        />
+                                        <DoctorExtraInfor
+                                            doctorIdFromParent={item} />
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
+            </div>
         );
     }
 }
